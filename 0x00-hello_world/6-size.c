f6afb8e92c3a3c5e5fb89847d1e3d05d25c8d425
Kyekyeku <safo.oppong@hotmail.com>
2023-07-13 15:48:12 -0700

    My 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 /**
- * main A program which prints the size of various computer types
- * Return 0 (Success)
+ * main - A program which prints the size of various computer types
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of short: %zu bytes\n", sizeof(short));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long: %zu bytes\n", sizeof(long));
-printf("Size of long long: %zu bytes\n", sizeof(long long));
-printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
-printf("Size of long double: %zu bytes\n", sizeof(long double));
+char a;
+short b;
+int c;
+long int d;
+long long e;
+float f;
+double g;
+long double h;
+
+printf("Size of char: %zu bytes\n", sizeof(a));
+printf("Size of short: %zu bytes\n", sizeof(b));
+printf("Size of int: %zu bytes\n", sizeof(c));
+printf("Size of long: %zu bytes\n", sizeof(d));
+printf("Size of long long: %zu bytes\n", sizeof(e));
+printf("Size of float: %zu bytes\n", sizeof(f));
+printf("Size of double: %zu bytes\n", sizeof(g));
+printf("Size of long double: %zu bytes\n", sizeof(h));
 return (0);
 }
