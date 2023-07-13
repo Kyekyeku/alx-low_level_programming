891bd0280b1141588fbd0f937f315e0062f91328
Kyekyeku <safo.oppong@hotmail.com>
2023-07-13 15:57:49 -0700

    my 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,22 +5,12 @@
  */
 int main(void)
 {
-char a;
-short b;
-int c;
-long int d;
-long long e;
-float f;
-double g;
-long double h;
-
-printf("Size of char: %zu bytes\n", sizeof(a));
-printf("Size of short: %zu bytes\n", sizeof(b));
-printf("Size of int: %zu bytes\n", sizeof(c));
-printf("Size of long: %zu bytes\n", sizeof(d));
-printf("Size of long long: %zu bytes\n", sizeof(e));
-printf("Size of float: %zu bytes\n", sizeof(f));
-printf("Size of double: %zu bytes\n", sizeof(g));
-printf("Size of long double: %zu bytes\n", sizeof(h));
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of short: %zu byte(s)\n",sizeof(short));
+printf("Size of long: %zu byte(s)\n", sizeof(long));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
+printf("Size of double: %zu byte(s)\n", sizeof(double));
+printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 return (0);
 }
