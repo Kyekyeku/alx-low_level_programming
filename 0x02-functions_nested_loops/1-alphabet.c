237ce989319a78ddcad8a43e649738511f65f8c7
root <safo.oppong@hotmail.com>
2023-09-12 05:07:57 -0700

    my 1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+	_putchar(s);
+	}
+	_putchar('\n');
+}
