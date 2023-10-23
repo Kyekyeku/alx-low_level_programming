b8a33385507c389af0bd69a1ea485f95df13466d
root <safo.oppong@hotmail.com>
2023-10-23 09:31:54 -0700

    my 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -10,11 +10,11 @@
  */
 int main(void)
 {
-	istint_t *head;
+	listint_t *head;
 	listint_t *new;
 	listint_t hello = {8, NULL};
 	size_t n;
-	
+
 	head = &hello;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
